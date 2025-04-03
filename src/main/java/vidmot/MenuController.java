1e182d9431fd4ee5546df0ed4d49e8838837af89
0xFEE1DEAD2 <nullbytephantom@protonmail.com>
2025-04-03 12:00:47 +0000

    StackPane virkni löguð

--- src/main/java/vidmot/MenuController.java
+++ src/main/java/vidmot/MenuController.java
@@ -2,9 +2,23 @@ package vidmot;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.Node;
+
+import java.io.IOException;
 
 public class MenuController {
+
+    @FXML
+    private StackPane stackPaneCenter;
+    @FXML
+    private BorderPane rootBorderPane;
+    @FXML
+    private VBox centerVBox;
 
     @FXML
     private Button buttonAustur;
@@ -23,53 +37,68 @@ public class MenuController {
     @FXML
     private Button fxTilPlast;
 
+    @FXML
+    public void initialize() {
+        // Load default view on app start
+        setView("velkominn-view.fxml");
+    }
+
+    private void setView(String fxmlFile) {
+        try {
+            Node view = FXMLLoader.load(getClass().getResource(fxmlFile));
+            stackPaneCenter.getChildren().setAll(view);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public void fxFaraAustur(ActionEvent event) {
         // Fara í Austur
-        ViewSwitcher.switchTo(View.AUSTUR);
+        setView("hvertAustur-view.fxml");
         System.out.println("Þessi takki fer í Austur");
     }
 
     public void fxFaraHofud(ActionEvent event) {
         // Fara í Höfuðborgarsvæðið
-        ViewSwitcher.switchTo(View.HOFUD);
+        setView("hvertHofud-view.fxml");
         System.out.println("Þessi takki fer í Höfuðborgarsvæðið");
     }
 
     public void fxFaraNordur(ActionEvent event) {
         // Fara í Norður
-        ViewSwitcher.switchTo(View.NORDUR);
+        setView("hvertNordur-view.fxml");
         System.out.println("Þessi takki fer í Norður");
     }
 
     public void fxFaraSudur(ActionEvent event) {
         // Fara í Suður
-        ViewSwitcher.switchTo(View.SUDUR);
+        setView("hvertSudur-view.fxml");
         System.out.println("Þessi takki fer í Suður");
     }
 
     public void fxFaraVestur(ActionEvent event) {
         // Fara í Vestur
-        ViewSwitcher.switchTo(View.VESTUR);
+        setView("hvertVestur-view.fxml");
         System.out.println("Þessi takki fer í Vestur");
     }
 
     public void fxTilAl(ActionEvent event) {
-        ViewSwitcher.switchTo(View.ÁL);
+        setView("maSkilaAl-view.fxml");
         System.out.println("Þessi takki fer í ál síðuna");
     }
 
     public void fxTilGler(ActionEvent event) {
-        ViewSwitcher.switchTo(View.GLER);
+        setView("maSkilaGler-view.fxml");
         System.out.println("Þessi takki fer í gler síðuna");
     }
 
     public void fxTilPlast(ActionEvent event) {
-        ViewSwitcher.switchTo(View.PLAST);
+        setView("maSkilaPlast-view.fxml");
         System.out.println("Þessi takki fer í plast síðuna");
     }
 
     public void fxTelja(ActionEvent event) {
-        ViewSwitcher.switchTo(View.TELJA);
+        setView("floskur-view.fxml");
         System.out.println("Þessi takki fer í telja síðuna");
     }
 }
